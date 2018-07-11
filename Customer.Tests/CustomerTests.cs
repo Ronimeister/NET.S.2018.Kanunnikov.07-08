cbc064d5a86cc086fb3f5bfacce5c1ab2d2f0ad7
Ronimeister <ronimeisterhs@gmail.com>
2018-07-11 15:37:43 +0300

    Project added

---
+++ Customer.Tests/CustomerTests.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomerLib;
+using System.Globalization;
+using FormatProviderLib;
+
+namespace Customer.Tests
+{
+    [TestFixture]
+    public class CustomerTests
+    {
+        #region Customer.cs tests
+        [TestCase("G", ExpectedResult = "Customer record: Jeffrey Richter, 1,234,567.00, +1 (425) 555-0100")]
+        [TestCase("NRP", ExpectedResult = "Customer record: Jeffrey Richter, 1,234,567.00, +1 (425) 555-0100")]
+        [TestCase("NR", ExpectedResult = "Customer record: Jeffrey Richter, 1,234,567.00")]
+        [TestCase("NP", ExpectedResult = "Customer record: Jeffrey Richter, +1 (425) 555-0100")]
+        [TestCase("RP", ExpectedResult = "Customer record: 1,234,567.00, +1 (425) 555-0100")]
+        [TestCase("N", ExpectedResult = "Customer record: Jeffrey Richter")]
+        [TestCase("R", ExpectedResult = "Customer record: 1,234,567.00")]
+        [TestCase("P", ExpectedResult = "Customer record: +1 (425) 555-0100")]
+        [TestCase(null, ExpectedResult = "Customer record: Jeffrey Richter, 1,234,567.00, +1 (425) 555-0100")]
+        [TestCase("", ExpectedResult = "Customer record: Jeffrey Richter, 1,234,567.00, +1 (425) 555-0100")]
+        public string ToString_IsCorrect(string format)
+        {
+            CustomerLib.Customer customer = new CustomerLib.Customer("Jeffrey Richter", 1234567, "+1 (425) 555-0100");
+            return customer.ToString(format);
+        }
+
+        [TestCase("G", "ru-RU", ExpectedResult = "Customer record: Jeffrey Richter, 1 234 567,00, +1 (425) 555-0100")]
+        [TestCase("NRP", "en-US", ExpectedResult = "Customer record: Jeffrey Richter, 1,234,567.00, +1 (425) 555-0100")]
+        [TestCase("NR", "ru-RU", ExpectedResult = "Customer record: Jeffrey Richter, 1 234 567,00")]
+        [TestCase("NP", "en-US", ExpectedResult = "Customer record: Jeffrey Richter, +1 (425) 555-0100")]
+        [TestCase("RP", "ru-RU", ExpectedResult = "Customer record: 1 234 567,00, +1 (425) 555-0100")]
+        [TestCase("N", "ru-RU", ExpectedResult = "Customer record: Jeffrey Richter")]
+        [TestCase("R", "en-US", ExpectedResult = "Customer record: 1,234,567.00")]
+        [TestCase("P", "ru-RU", ExpectedResult = "Customer record: +1 (425) 555-0100")]
+        [TestCase(null, "ru-RU", ExpectedResult = "Customer record: Jeffrey Richter, 1 234 567,00, +1 (425) 555-0100")]
+        [TestCase("", "en-US", ExpectedResult = "Customer record: Jeffrey Richter, 1,234,567.00, +1 (425) 555-0100")]
+        public string ToString_IsCorrect_WithCulture(string format, string culture)
+        {
+            CultureInfo cultureInfo = new CultureInfo(culture);
+            CustomerLib.Customer customer = new CustomerLib.Customer("Jeffrey Richter", 1234567, "+1 (425) 555-0100");
+            return customer.ToString(format, cultureInfo);
+        }
+
+
+        [TestCase("GSDSD")]
+        [TestCase("OG")]
+        public void ToString_FormatIsNotSupported_FormatException(string format)
+            => Assert.Throws<FormatException>(() => new CustomerLib.Customer().ToString(format));
+        #endregion
+
+        #region CustomerFormatProvider.cs tests
+        [TestCase("GC", ExpectedResult = "Customer record: Jeffrey Richter, ¤1,234,567.00, +1 (425) 555-0100")]
+        [TestCase("NRCP", ExpectedResult = "Customer record: Jeffrey Richter, ¤1,234,567.00, +1 (425) 555-0100")]
+        [TestCase("RC", ExpectedResult = "Customer record: ¤1,234,567.00")]
+        [TestCase("NRC", ExpectedResult = "Customer record: Jeffrey Richter, ¤1,234,567.00")]
+        [TestCase("RCP", ExpectedResult = "Customer record: ¤1,234,567.00, +1 (425) 555-0100")]
+        [TestCase("W", ExpectedResult = "Customer record: Jeffrey Richter, one two three four five six seven, one four two five five five five minus zero one zero zero")]
+        [TestCase("RW", ExpectedResult = "Customer record: one two three four five six seven")]
+        [TestCase("PW", ExpectedResult = "Customer record: one four two five five five five minus zero one zero zero")]
+        [TestCase("NPW", ExpectedResult = "Customer record: Jeffrey Richter, one four two five five five five minus zero one zero zero")]
+        [TestCase("NRW", ExpectedResult = "Customer record: Jeffrey Richter, one two three four five six seven")]
+        public string Format_IsCorrect(string format)
+        {
+            CustomerFormatProvider provider = new CustomerFormatProvider();
+            return provider.Format(format, new CustomerLib.Customer("Jeffrey Richter", 1234567, "+1 (425) 555-0100"), CultureInfo.InvariantCulture);
+        }
+
+        [TestCase("GC", "ru-RU", ExpectedResult = "Customer record: Jeffrey Richter, 1 234 567,00 ₽, +1 (425) 555-0100")]
+        [TestCase("NRCP", "en-US", ExpectedResult = "Customer record: Jeffrey Richter, $1,234,567.00, +1 (425) 555-0100")]
+        [TestCase("RC", "ru-RU", ExpectedResult = "Customer record: 1 234 567,00 ₽")]
+        [TestCase("NRC", "en-US", ExpectedResult = "Customer record: Jeffrey Richter, $1,234,567.00")]
+        [TestCase("RCP", "ru-RU", ExpectedResult = "Customer record: 1 234 567,00 ₽, +1 (425) 555-0100")]
+        [TestCase("W", "ru-RU", ExpectedResult = "Customer record: Jeffrey Richter, one two three four five six seven, one four two five five five five minus zero one zero zero")]
+        [TestCase("RW", "en-US", ExpectedResult = "Customer record: one two three four five six seven")]
+        [TestCase("PW", "ru-RU", ExpectedResult = "Customer record: one four two five five five five minus zero one zero zero")]
+        [TestCase("NPW", "ru-RU", ExpectedResult = "Customer record: Jeffrey Richter, one four two five five five five minus zero one zero zero")]
+        [TestCase("NRW", "en-US", ExpectedResult = "Customer record: Jeffrey Richter, one two three four five six seven")]
+        public string Format_IsCorrect_WithCulture(string format, string culture)
+        {
+            CustomerFormatProvider provider = new CustomerFormatProvider();
+            return provider.Format(format, new CustomerLib.Customer("Jeffrey Richter", 1234567, "+1 (425) 555-0100"), new CultureInfo(culture));
+        }
+
+        [TestCase("DRERE")]
+        [TestCase("HITHERE")]
+        public void Format_FormatIsNotSupported_FormatException(string format)
+            => Assert.Throws<FormatException>(() => new CustomerFormatProvider().Format(format, new CustomerLib.Customer("Jeffrey Richter", 1234567, "+1 (425) 555-0100"), CultureInfo.InvariantCulture));
+        #endregion
+    }
+}
---
+++ CustomerLib/Customer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Globalization;
+
+namespace CustomerLib
+{
+    /// <summary>
+    /// Class that provides information about customer
+    /// </summary>
+    public class Customer : IFormattable
+    {
+        #region Constants
+        private const string FORMAT_BY_DEFAULT = "G";
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Public property that describe customer name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Public property that describe customer contact phone
+        /// </summary>
+        public string ContactPhone { get; set; }
+
+        /// <summary>
+        /// Public property that describe customer revenue
+        /// </summary>
+        public decimal Revenue { get; set; }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Default <see cref="Customer"/> constructor
+        /// </summary>
+        public Customer()
+        {
+            Name = string.Empty;
+            ContactPhone = string.Empty;
+            Revenue = 0;
+        }
+
+        /// <summary>
+        /// <see cref="Customer"/> constructor
+        /// </summary>
+        /// <param name="name">Describe customer name</param>
+        /// <param name="revenue">Describe customer revenue</param>
+        /// <param name="contactPhone">Describe customer phone</param>
+        public Customer(string name, decimal revenue, string contactPhone)
+        {
+            Name = name;
+            ContactPhone = contactPhone;
+            Revenue = revenue;
+        }
+        #endregion
+
+        #region ToString()
+        /// <summary>
+        /// Overrided <see cref="Object.ToString"/> method
+        /// </summary>
+        /// <returns>Default <see cref="Customer"/> string represantation</returns>
+        public override string ToString()
+            => $"Customer record: {Name} {Revenue.ToString("N", CultureInfo.InvariantCulture)} {ContactPhone}";
+
+        /// <summary>
+        /// <see cref="Customer"/> string represantation
+        /// </summary>
+        /// <param name="format">String format</param>
+        /// <returns><see cref="Customer"/> string represantation based on <paramref name="format"/></returns>
+        public string ToString(string format) => ToString(format, CultureInfo.InvariantCulture);
+
+        /// <summary>
+        /// <see cref="Customer"/> string represantation
+        /// </summary>
+        /// <param name="format">String format</param>
+        /// <param name="formatProvider">String format provider</param>
+        /// <exception cref="FormatException">Throws when <paramref name="format"/> is not supported</exception>
+        /// <returns><see cref="Customer"/> string represantation based on <paramref name="format"/> and <paramref name="formatProvider"/></returns>
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (string.IsNullOrEmpty(format))
+            {
+                format = FORMAT_BY_DEFAULT;
+            }
+
+            if (formatProvider is null)
+            {
+                formatProvider = CultureInfo.InvariantCulture;
+            }
+
+            return GenerateStringByFormat(format, formatProvider);
+        }
+        #endregion
+
+        #region Private API
+        /// <summary>
+        /// Generate <see cref="Customer"/> string based on <paramref name="format"/> and <paramref name="formatProvider"/>
+        /// </summary>
+        /// <param name="format">String format</param>
+        /// <param name="formatProvider">String format provider</param>
+        /// <returns><see cref="Customer"/> string represantation based on <paramref name="format"/> and <paramref name="formatProvider"/></returns>
+        private string GenerateStringByFormat(string format, IFormatProvider formatProvider)
+        {
+            switch (format.Trim().ToUpperInvariant())
+            {
+                case "G":
+                case "NRP":
+                    return $"Customer record: {Name}, {Revenue.ToString("N", formatProvider)}, {ContactPhone}";
+                case "N":
+                    return $"Customer record: {Name}";
+                case "R":
+                    return $"Customer record: {Revenue.ToString("N", formatProvider)}";
+                case "P":
+                    return $"Customer record: {ContactPhone}";
+                case "NR":
+                    return $"Customer record: {Name}, {Revenue.ToString("N", formatProvider)}";
+                case "NP":
+                    return $"Customer record: {Name}, {ContactPhone}";
+                case "RP":
+                    return $"Customer record: {Revenue.ToString("N", formatProvider)}, {ContactPhone}";
+                default:
+                    throw new FormatException($"{nameof(format)} is not supported.");
+            }
+        }
+        #endregion
+    }
+}
